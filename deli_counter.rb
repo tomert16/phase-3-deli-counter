09d8a4ca09ace9f8b595474acc47645e42ba5c15
tomer <tomertal52@yahoo.com>
2023-01-12 15:29:47 -0500

    commit tree 3bd97fcef9dacdb55c09713a99168abc030259c3

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+katz_deli = ["Avi", "Logan", "Ada"];
+
+def line(katz_deli) 
+    if katz_deli.size > 1
+        line_s = katz_deli.map.with_index(1) { |customer, number|  "#{number}. #{customer}"}
+        puts "The line is currently: " + line_s.join(" ")
+    elsif katz_deli.size == 0
+        puts "The line is currently empty."
+    end
+end 
+
+line(katz_deli)
+
+def take_a_number(katz_deli, customer)
+    katz_deli << customer
+    puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
+end
+
+take_a_number(katz_deli, "Tomer")
+take_a_number(katz_deli, "Courtney")
+take_a_number(katz_deli, "Greg")
+
+def now_serving(katz_deli)
+    if katz_deli.size == 0 
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    end
+end
+now_serving(katz_deli)
+now_serving(katz_deli)
